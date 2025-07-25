9376946e1c3d676257acc4c5fdd5d65021e7cee2
Saurabh <saurabhkr0927@gmail.com>
2025-07-25 13:58:44 +0530

    set cookie to browser after sigin with google

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -20,6 +20,8 @@ export default function OAuth() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
+
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
